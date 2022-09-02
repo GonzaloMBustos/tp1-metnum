21946a2314611a99e1eb6f4e4b978b9d9371919c
Alexis <alexisbalbachan@gmail.com>
2022-09-02 15:39:31 -0300

    actualizado main para llamar a EG

--- src/main_alexis.cpp
+++ src/main_alexis.cpp
@@ -110,6 +110,8 @@ void basicTesting()
     cout << "--------------Matriz 1 * Matriz 2--------------" << endl;
     sm4b41 *sm4b42;
     sm4b41.showMatrix(cout);
+    //vector<double> b = {0,0,0,0};
+    //sm4b41.EG(b);
 
 
     SparseMatrixReloaded sm2b2(2, 2, 0.0001);
@@ -122,17 +124,38 @@ void basicTesting()
     sm2b2.showMatrix(cout);
 
     SparseMatrixReloaded sm2b22(2, 2, 0.0001);
-    sm2b22[1][1] = 1;
+    sm2b22[1][1] = 2;
     sm2b22[1][2] = 1;
     sm2b22[2][1] = 1;
-    sm2b22[2][2] = 1;
+    sm2b22[2][2] = 4;
 
     cout << "--------------Matriz 2 de 2x2--------------" << endl;
+    sm2b22.showMatrix(cout);
+
+    vector<double> b = {0,0};
+    sm2b22.EG(b);
     sm2b22.showMatrix(cout);
 
     cout << "==============Matrix sum===================" << endl;
     sm2b2 + sm2b22;
     sm2b2.showMatrix(cout);
+
+
+    sm3b32[1][1] = 2;
+    sm3b32[1][2] = 1;
+    sm3b32[1][3] = 3;
+    sm3b32[2][1] = 2;
+    sm3b32[2][2] = 2;
+    sm3b32[2][3] = 3;
+    sm3b32[3][1] = 2;
+    sm3b32[3][2] = 3;
+    sm3b32[3][3] = 0;
+    sm3b32.showMatrix(cout);
+
+    vector<double> b2 = {1,2,4.5};
+    sm3b32.EG(b2);
+    sm3b32.showMatrix(cout);
+    cout << "[" << b2[0] << ", " << b2[1] << ", " << b2[2] << "]" << endl;
     
 }
 
@@ -158,3 +181,4 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
